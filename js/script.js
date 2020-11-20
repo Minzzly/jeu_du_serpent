35bf9847d4c7d79670ed9b5da24db6f962a881e4
Audrey Langlois <azpmaggie@gmail.com>
2020-11-20 16:02:11 -0500

    Excelent endoit pour faire commit-- creer trois class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
+    //Le jeu
+
+    class Jeu{
+        constructor() {
+            console.log("Creation du Jeu");
+        }
+    }
+    //Le serpent
+
+    class Serpent{
+        constructor(){
+            console.log("Creation du serpent");
+        }
+    }
+
+    //La pomme
+    class Pomme{
+        constructor(){
+            console.log("Creation de la pomme");
+        }
+    }
+
+    var unePartie=new Jeu();
 
 
 });
