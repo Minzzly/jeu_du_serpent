13a71abd2bd49191f671f41ab201f10599a8c82e
Audrey Langlois <azpmaggie@gmail.com>
2020-11-20 16:50:38 -0500

    Excelent endoit pour faire commit-- fait en sorte que quand on appuit sur le bouton jouer, il fait des console.log pour commencer la partie.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Le jeu
 
     class Jeu{
-        constructor() {
+        constructor(_idSvg, _idPointage){
             console.log("Creation du Jeu");
+            this.s = Snap(_idSvg);
+            this.sortiPointage = document.querySelector(_idPointage);
+            this.grandeurCarre = 20;
+            this.grandeurGrille = 15;
+
+        }
+
+        nouvellePartie(){
+            this.affichagePointage(1);
+            this.pomme = new Pomme ();
+            this.serpent = new Serpent();
+
+        }
+
+        finPartie(){
+
+        }
+
+        affichagePointage(_lePointage){
+            this.sortiPointage.innerHTML = _lePointage;
+
         }
     }
     //Le serpent
@@ -21,7 +42,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
-    var unePartie=new Jeu();
+    var unePartie= new Jeu("#jeu", "#pointage");
 
+    var btnJouer = document.querySelector("#btnJouer");
+    btnJouer.addEventListener("click", nouvellePartie);
+
+    function nouvellePartie(){
+        unePartie.nouvellePartie();
+    }
 
 });
